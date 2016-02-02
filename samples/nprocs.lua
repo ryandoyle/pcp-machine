a448d3555ab3aafff667773b7af6d7db4ebe1ba7
Ryan Doyle <ryan@doylenet.net>
2016-02-02 14:40:03 +1100

    Add some useful documentation to sample Lua script

--- samples/nprocs.lua
+++ samples/nprocs.lua
@@ -1,4 +1,7 @@
+-- Will query for a metric against the host specified via -H option
 nprocs = metric('kernel.all.nprocs')
 if nprocs > 100 then
   print('Processes are too high!:' .. nprocs)
 end
+
+-- If no return is given, pcp-machine will exit with an exit status of 0
